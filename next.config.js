47cfd213fcc8a40c6550813bd7b5f718b28d26e4
Lucas Eitel <leitel1@uc.cl>
2023-03-19 21:54:05 -0300

    aaaaaa

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     domains: ['raw.githubusercontent.com'],
     unoptimized: true,
+    basePath: '/My_Pokemon_List',
+    assetPrefix: '/My_Pokemon_List/',
   }
 }
 
